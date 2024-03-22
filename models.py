b2672c1b16c7f63e992911e9b64726949f028032
germanadrianGIT <121576067+germanadrianGIT@users.noreply.github.com>
2024-03-22 15:54:14 -0300

    save and checkpoint model trained, bad test results

--- models.py
+++ models.py
@@ -89,9 +89,6 @@ class Net(nn.Module):
         # 3 Activation1 (32, 93, 93)
         # 4 Maxpooling2d1 (32, 46, 46)
         # 5 Dropout1 (32, 46, 46)
-        
-        print_tensor_info(x)
-        
         x = self.pool(F.relu(self.conv1(x)))
         x = self.dropout(x)        
                 
@@ -99,8 +96,6 @@ class Net(nn.Module):
         # 7 Activation2 (64, 44, 44)
         # 8 Maxpooling2d2 (64, 22, 22)
         # 9 Dropout2 (64, 22, 22)
-        print_tensor_info(x)
-        
         x = self.pool(F.relu(self.conv2(x)))
         x = self.dropout(x)
        
@@ -108,8 +103,6 @@ class Net(nn.Module):
         # 11 Activation3 (128, 21, 21)
         # 12 Maxpooling2d3 (128, 10, 10)
         # 13 Dropout3 (128, 10, 10)
-        print_tensor_info(x)
-        
         x = self.pool(F.relu(self.conv3(x)))
         x = self.dropout(x)
         
@@ -117,40 +110,28 @@ class Net(nn.Module):
         # 15 Activation4 (256, 10, 10)
         # 16 Maxpooling2d4 (256, 5, 5)
         # 17 Dropout4 (256, 5, 5)
-        print_tensor_info(x)
-        
         x = self.pool(F.relu(self.conv4(x)))
         x = self.dropout(x)
         
         # 18 Flatten1 (6400)
         # Flatten the tensor for fully connected layers
-        print_tensor_info(x)
-        
         x = x.view(-1, 256 * 5 * 5)
 
         # Apply fully connected layers with ReLU activation and dropout
         # 19 Dense1 (1000)
         # 20 Activation5 (1000)
         # 21 Dropout5 (1000)
-        print_tensor_info(x)
-        
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         
         # 22 Dense2 (1000)
         # 23 Activation6 (1000)
         # 24 Dropout6 (1000)
-        print_tensor_info(x)
-        
         x = F.relu(self.fc2(x))
         x = self.dropout(x)
         
         # 25 Dense3 (2)
-        print_tensor_info(x)
-        
         x = self.fc3(x)
-        
-        print_tensor_info(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
