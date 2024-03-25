5adc2d9ce40b0ef9dd8b2a5739d7c256eaa8a3d7
germanadrianGIT <121576067+germanadrianGIT@users.noreply.github.com>
2024-03-25 16:28:39 -0300

    CNN changes
    # self.conv1 = nn.Conv2d(1, 32, 4), 30 epochs

--- models.py
+++ models.py
@@ -33,39 +33,43 @@ class Net(nn.Module):
         # F is the filter size (in this case, the width of the convolutional filter/kernel).
         # S is the stride (the number of pixels by which we move the filter/kernel across the input image).        
         
+        
+        
+        
+        
         # 1 input image channel (grayscale), 32 output channels/feature maps, 4x4 square convolution kernel
-        ## output size = (W-F)/S +1 = (96-4)/1 +1 = 93
-        # the output Tensor for one image, will have the dimensions: (32, 93, 93)
-        # after one pool layer, this becomes (32, 46, 46); 46.5 is rounded down
+        ## output size = (W-F)/S +1 = (224-4)/1 +1 = 221
+        # the output Tensor for one image, will have the dimensions: (32, 221, 221)
+        # after one pool layer, this becomes (32, 110, 110); 110.5 is rounded down
         self.conv1 = nn.Conv2d(1, 32, 4)  # Convolutional layer 1
         
         # second conv layer: 32 inputs, 64 outputs, 3x3 conv
-        ## output size = (W-F)/S +1 = (46-3)/1 +1 = 44
-        # the output Tensor for one image, will have the dimensions: (64, 44, 44)
-        # after one pool layer, this becomes (64, 22, 22);        
+        ## output size = (W-F)/S +1 = (110-3)/1 +1 = 108
+        # the output Tensor for one image, will have the dimensions: (64, 108, 108)
+        # after one pool layer, this becomes (64, 54, 54);        
         self.conv2 = nn.Conv2d(32, 64, 3)  # Convolutional layer 2
         
         
         # third conv layer: 64 inputs, 128 outputs, 2x2 conv
-        ## output size = (W-F)/S +1 = (22-2)/1 +1 = 21
-        # the output Tensor for one image, will have the dimensions: (128, 21, 21)
-        # after one pool layer, this becomes (128, 10, 10); 10.5 is rounded down        
+        ## output size = (W-F)/S +1 = (54-2)/1 +1 = 53
+        # the output Tensor for one image, will have the dimensions: (128, 53, 53)
+        # after one pool layer, this becomes (128, 26, 26); 26.5 is rounded down        
         self.conv3 = nn.Conv2d(64, 128, 2)  # Convolutional layer 3
         
         # fourth conv layer: 128 inputs, 256 outputs, 1x1 conv
-        ## output size = (W-F)/S +1 = (10-1)/1 +1 = 10
-        # the output Tensor for one image, will have the dimensions: (256, 10, 10)
-        # after one pool layer, this becomes (256, 5, 5);        
+        ## output size = (W-F)/S +1 = (26-1)/1 +1 = 26
+        # the output Tensor for one image, will have the dimensions: (256, 26, 26)
+        # after one pool layer, this becomes (256, 13, 13);        
         self.conv4 = nn.Conv2d(128, 256, 1)  # Convolutional layer 4
         
         # maxpool layer
         # pool with kernel_size=2, stride=2
         self.pool = nn.MaxPool2d(2, 2)
         
-        # 256 inputs * the 5*5 filtered/pooled map size
+        # 256 inputs * the 4*4 filtered/pooled map size
         # 1000 output channels
         
-        self.fc1 = nn.Linear(256 * 5 * 5, 1000)  # Fully connected layer 1
+        self.fc1 = nn.Linear(256 * 13 * 13, 1000)  # Fully connected layer 1
         
         # 1000 inputs, 1000 output channels
         self.fc2 = nn.Linear(1000, 1000)  # Fully connected layer 2
@@ -84,39 +88,44 @@ class Net(nn.Module):
         #Layer Number, Layer Name, Layer Shape
 
         # Apply convolutional layers with ReLU activation and pooling
-        # 1 Input1 (1, 96, 96)
-        # 2 Convolution2d1 (32, 93, 93)***
-        # 3 Activation1 (32, 93, 93)
-        # 4 Maxpooling2d1 (32, 46, 46)
-        # 5 Dropout1 (32, 46, 46)
+        # 1 Input1 (1, 224, 224)
+        
+        # 2 Convolution2d1 (32, 221, 221)***
+        # 3 Activation1 (32, 221, 221)
+        # 4 Maxpooling2d1 (32, 110, 110)
+        # 5 Dropout1 (32, 110, 110)
         x = self.pool(F.relu(self.conv1(x)))
         x = self.dropout(x)        
                 
-        # 6 Convolution2d2 (64, 44, 44)***
-        # 7 Activation2 (64, 44, 44)
-        # 8 Maxpooling2d2 (64, 22, 22)
-        # 9 Dropout2 (64, 22, 22)
+        # 6 Convolution2d2 (64, 108, 108)***
+        # 7 Activation2 (64, 108, 108)
+        # 8 Maxpooling2d2 (64, 54, 54)
+        # 9 Dropout2 (64, 54, 54)
         x = self.pool(F.relu(self.conv2(x)))
         x = self.dropout(x)
        
-        # 10 Convolution2d3 (128, 21, 21)***
-        # 11 Activation3 (128, 21, 21)
-        # 12 Maxpooling2d3 (128, 10, 10)
-        # 13 Dropout3 (128, 10, 10)
+        # 10 Convolution2d3 (128, 53, 53)***
+        # 11 Activation3 (128, 53, 53)
+        # 12 Maxpooling2d3 (128, 26, 26)
+        # 13 Dropout3 (128, 26, 26)
         x = self.pool(F.relu(self.conv3(x)))
         x = self.dropout(x)
         
-        # 14 Convolution2d4 (256, 10, 10)***
-        # 15 Activation4 (256, 10, 10)
-        # 16 Maxpooling2d4 (256, 5, 5)
-        # 17 Dropout4 (256, 5, 5)
+        # 14 Convolution2d4 (256, 26, 26)***
+        # 15 Activation4 (256, 26, 26)
+        # 16 Maxpooling2d4 (256, 13, 13)
+        # 17 Dropout4 (256, 13, 13)
         x = self.pool(F.relu(self.conv4(x)))
         x = self.dropout(x)
         
-        # 18 Flatten1 (6400)
+#         print_tensor_info(x)
+        
+        # 18 Flatten1 (43264)
         # Flatten the tensor for fully connected layers
-        x = x.view(-1, 256 * 5 * 5)
-
+        x = x.view(-1, 256 * 13 * 13)
+        
+#         print_tensor_info(x)
+        
         # Apply fully connected layers with ReLU activation and dropout
         # 19 Dense1 (1000)
         # 20 Activation5 (1000)
@@ -130,7 +139,7 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.dropout(x)
         
-        # 25 Dense3 (2)
+        # 25 Dense3 (136)
         x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
